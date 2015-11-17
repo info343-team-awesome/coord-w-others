94f77afc13d6d3fd453d48b18ffad37660916311
rohan <rohanaras@gmail.com>
2015-11-16 20:09:49 -0800

    created dob field

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 
 angular.module('SignUp', [])
+    .directive('inThePast', function() {
+        "use strict";
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) {
+                    var today = new Date();
+                    return (new Date(modelValue) <= today);
+                }
+            }
+        };
+    })
     .controller('SignUpController', function($scope) {
         'use strict';
 
